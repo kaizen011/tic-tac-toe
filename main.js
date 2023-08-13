c9179849ab21537228fbad018679bde0891937a4
Kaizen <125158784+kaizen011@users.noreply.github.com>
2023-08-13 07:46:46 -0500

    Completed Module and Factory approach

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
-const Gameboard = (() => {
-
+// Player Factory
+function createPlayer(name, marker) {
+    return { name, marker };
+  }
+  
+  // Gameboard Module
+  const Gameboard = (() => {
     let board = [];
   
     const createBoard = () => {
-      board = Array(9).fill(null); // Created an empty game board
+      board = Array(9).fill(null);
     };
   
     const getBoard = () => {
@@ -13,17 +18,55 @@ const Gameboard = (() => {
     const placeMarker = (index, marker) => {
       if (board[index] === null) {
         board[index] = marker;
-        return true; // Marker placed successfully
+        return true;
       } else {
-        return false; // Cell already occupied
+        return false;
       }
     };
   
-    // Return public functions
     return {
       createBoard,
       getBoard,
       placeMarker,
     };
   })();
+  
+  // Game Object
+  const Game = (() => {
+    let currentPlayer;
+    let gameActive = false;
+  
+    const switchPlayer = () => {
+      currentPlayer = currentPlayer === player1 ? player2 : player1;
+    };
+  
+    const startGame = () => {
+      currentPlayer = player1;
+      gameActive = true;
+    };
+  
+    const endGame = () => {
+      gameActive = false;
+    };
+  
+    const handleCellClick = (index) => {
+      if (!gameActive) return;
+  
+      if (Gameboard.placeMarker(index, currentPlayer.marker)) {
+        switchPlayer();
+      }
+    };
+  
+    return {
+      startGame,
+      endGame,
+      handleCellClick,
+    };
+  })();
+  
+  // Generating Player Objects
+  const player1 = createPlayer("Player 1", "X");
+  const player2 = createPlayer("Player 2", "O");
+  
+ 
   
